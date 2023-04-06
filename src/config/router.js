1674ff2e669c24a71a4311c126285dec044087b2
Michael Ekwere <michaelgatesjr@gmail.com>
2023-04-06 21:57:01 +0100

    removed code that creates problems

--- src/config/router.js
+++ src/config/router.js
@@ -1,11 +1,6 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -13,41 +8,34 @@ import SignUp from "../pages/SignUp";
 import { firebaseAuth } from "../service/firebase";
 
 const RouterComponent = () => {
-  const [user, setUser] = useState();
-  useEffect(() => {
-    const User = () => {
-      onAuthStateChanged(firebaseAuth, async (user) => {
-        if (user) {
-          // User is signed in, see docs for a list of available properties
-          // https://firebase.google.com/docs/reference/js/firebase.User
-          const uid = await user.uid;
-          setUser(uid);
-          // ...
-        } else {
-          // User is signed out
-          // ...
-        }
-      });
-    };
-    User();
-  });
+	// const [user, setUser] = useState();
+	// useEffect(() => {
+	// 	const User = () => {
+	// 		onAuthStateChanged(firebaseAuth, async (user) => {
+	// 			if (user) {
+	// 				// User is signed in, see docs for a list of available properties
+	// 				// https://firebase.google.com/docs/reference/js/firebase.User
+	// 				const uid = await user.uid;
+	// 				setUser(uid);
+	// 				// ...
+	// 			} else {
+	// 				// User is signed out
+	// 				// ...
+	// 			}
+	// 		});
+	// 	};
+	// 	User();
+	// });
 
-
-  return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={user ? <Home /> : <SignUp />} />
-        {/* <Route path="/auth/signup" element={user ? <Navigate to="/" /> : <SignUp/> /> */}
-        <Route
-          path="/auth/login"
-          element={user ? <Navigate to="/" /> : <Login />}
-        />
-        <Route
-          path="*"
-          element={ <ErrorPage />}
-        />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/auth/signup" element={<SignUp />} />
+				<Route path="/auth/login" element={<Login />} />
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</Router>
+	);
 };
 export default RouterComponent;
